387052e5c62be8fd210b4a62ebb0c930886a5157
ndhuutai <30916803+ndhuutai@users.noreply.github.com>
2020-08-04 18:20:52 -0700

    Utils functions - class implementation. (#12)
    * added initial util functions
    
    * added babelrc only for jest
    
    * added initial test for a util function
    
    * added mattie functions and changed traverseWith
    
    * added jest transform for jest tests and react test renderer pkg
    
    * fixed the typo -_-
    
    * fixed mountToReactRoot
    
    * bumped up npm version
    
    * bumped version
    
    * wrapped implementation details in a class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const uniqueRandomArray = require('unique-random-array');
 const ourNames = require('./names.json');
+const ReactPinpoint = require('./utils/utils');
 
 module.exports = {
   all: ourNames,
   random: uniqueRandomArray(ourNames),
+  ReactPinpoint,
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,82 +1,86 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 
-let changes = [];
+class ReactPinpoint {
+  constructor() {
+    this.changes = [];
+  }
 
-function getCircularReplacer() {
-  const seen = new WeakSet();
-  return (key, value) => {
-    if (typeof value === 'object' && value !== null) {
-      if (seen.has(value)) {
-        return;
+  getCircularReplacer() {
+    const seen = new WeakSet();
+    return (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
       }
-      seen.add(value);
+      return value;
+    };
+  }
+
+  recordChangesToObjField(obj, field) {
+    Object.defineProperty(obj, field, {
+      get() {
+        return this._current;
+      },
+      set(value) {
+        this.changes.push(JSON.stringify(value, this.getCircularReplacer()));
+        this._current = value;
+      },
+    });
+  }
+
+  mountToReactRoot(reactRoot) {
+    // Reset changes
+    this.changes = [];
+    // Lift parent of react fibers tree
+    const parent = reactRoot._reactRootContainer._internalRoot;
+    const { current } = parent;
+    // Add listener to react fibers tree so changes can be recorded
+    this.recordChangesToObjField(parent, 'current');
+    // Reassign react fibers tree to record initial state
+    parent.current = current;
+  }
+
+  traverseWith(fiber, callback) {
+    callback(fiber);
+    if (fiber.child) {
+      this.traverseWith(fiber.child, callback);
     }
-    return value;
-  };
-}
-
-function recordChangesToObjField(obj, field) {
-  Object.defineProperty(obj, field, {
-    get() {
-      return this._current;
-    },
-    set(value) {
-      changes.push(JSON.stringify(value, getCircularReplacer()));
-      this._current = value;
-    },
-  });
-}
-
-function mountToReactRoot(reactRoot) {
-  // Reset changes
-  changes = [];
-  // Lift parent of react fibers tree
-  const parent = reactRoot._reactRootContainer._internalRoot;
-  const { current } = parent;
-  // Add listener to react fibers tree so changes can be recorded
-  recordChangesToObjField(parent, 'current');
-  // Reassign react fibers tree to record initial state
-  parent.current = current;
-}
-
-function traverseWith(fiber, callback) {
-  callback(fiber);
-  if (fiber.child) {
-    traverseWith(fiber.child, callback);
+    if (fiber.sibling) {
+      this.traverseWith(fiber.sibling, callback);
+    }
   }
-  if (fiber.sibling) {
-    traverseWith(fiber.sibling, callback);
+
+  flattenTree(tree) {
+    // Closured array for storing fibers
+    const arr = [];
+    // Closured callback for adding to arr
+    const callback = (fiber) => {
+      arr.push(fiber);
+    };
+    this.traverseWith(tree, callback);
+    return arr;
+  }
+
+  checkTime(fiber, threshold) {
+    return fiber.selfBaseDuration > threshold;
+  }
+
+  /**
+   *
+   * @param {number} threshold The rendering time to filter for.
+   */
+  getAllSlowComponentRenders(threshold, changesArray) {
+    const slowRenders = changesArray
+      .map(JSON.parse) // Convert objects to JSON
+      .map(this.flattenTree) // Flatten tree
+      .flat() // Flatten 2d array into 1d array
+      .filter((fiber) => this.checkTime(fiber, threshold)); // filter out all that don't meet threshold
+    // Return any times greater than 16ms
+    return slowRenders;
   }
 }
 
-function flattenTree(tree) {
-  // Closured array for storing fibers
-  const arr = [];
-  // Closured callback for adding to arr
-  const callback = (fiber) => {
-    arr.push(fiber);
-  };
-  traverseWith(tree, callback);
-  return arr;
-}
-
-function checkTime(fiber, threshold) {
-  return fiber.selfBaseDuration > threshold;
-}
-
-/**
- *
- * @param {number} threshold The rendering time to filter for.
- */
-function getAllSlowComponentRenders(threshold, changesArray) {
-  const slowRenders = changesArray
-    .map(JSON.parse) // Convert objects to JSON
-    .map(flattenTree) // Flatten tree
-    .flat() // Flatten 2d array into 1d array
-    .filter((fiber) => checkTime(fiber, threshold)); // filter out all that don't meet threshold
-  // Return any times greater than 16ms
-  return slowRenders;
-}
-
-module.exports = { mountToReactRoot, getAllSlowComponentRenders, traverseWith, getCircularReplacer };
+module.exports = { ReactPinpoint };
