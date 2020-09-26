a884f260a1124cb5f991b31bd07c3f6ae007be25
ndhuutai <30916803+ndhuutai@users.noreply.github.com>
2020-09-26 14:39:14 -0700

    New fiber implementation. (#25)
    * new implementation and testing folder
    
    * removed example folder, react-scripts caused dependecy issues
    
    * added new implementations and utils functions, 1-file structure right now since module doesn't work with Puppeteer eval

--- example/Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM buildkite/puppeteer:5.2.1
-
-WORKDIR /tests
-ADD . . 
-
-RUN npm ci 
-CMD [ "node", "src/__tests__/index.js" ]
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,93 +1,423 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-empty */
+
 let changes = [];
+const processedFibers = new WeakMap();
+const fiberMap = new Map();
+const testWeakSet = new WeakSet();
+const stateNodeWeakSet = new WeakSet();
 
-// used for parsing a node to a usable form (with )
-function parseNode(node) {
-  return {
-    type: node.type,
-    selfBaseDuration: node.selfBaseDuration,
-    state: node?.stateNode?.state?.next,
-    child: node.child && parseNode(node.child),
-    sibling: node.sibling && parseNode(node.sibling),
-  };
+class TreeNode {
+  // will add type after ;D
+  uID: any;
+  elementType: any;
+  selfBaseDuration: any;
+  memoizedProps: any;
+  memoizedState: any;
+  effectTag: any;
+  ref: any;
+  fiberName: any;
+  updateQueue: any;
+  tag: any;
+  updateList: any[];
+  stateNode:
+    | {
+        state: any;
+        updater: any;
+      }
+    | string;
+  child: any;
+  sibling: any;
+  constructor(fiberNode, uID) {
+    this.uID = uID;
+    const {elementType, selfBaseDuration, memoizedState, memoizedProps, effectTag, tag, ref, updateQueue, stateNode} = fiberNode;
+    this.elementType = elementType;
+    this.selfBaseDuration = selfBaseDuration;
+    this.memoizedProps = memoizedProps;
+    this.memoizedState = memoizedState;
+    this.effectTag = effectTag;
+    this.ref = ref;
+    this.fiberName = getElementName(fiberNode);
+    this.updateQueue = updateQueue; // seems to be replaced entirely and since it exists directly under a fiber node, it can't be modified.
+    this.tag = tag;
+    this.updateList = [];
+
+    // stateNode can contain circular references depends on the fiber node
+    if (tag === 5) {
+      this.stateNode = 'host component';
+    } else if (tag === 3) {
+      this.stateNode = 'host root';
+    } else {
+      this.stateNode = stateNode;
+    }
+
+    // check the fiber and the attach the spy
+    // find a way to store the update function variable and result here
+    // if ((tag === 1 || tag === 5) && this.stateNode && this.stateNode.state) {
+    //   /*
+    //     enqueueReplaceState (inst, payload, callback)
+    //     enqueueForceUpdate (inst, callback)
+    //     enqueueSetState    (inst, payload, callback)
+    //   */
+    //   if (this.stateNode.updater) {
+    //     const cb = (update, payload) => {
+    //       this.updateList.push([update, payload]);
+    //     };
+    //     // spying
+    //     // if (!stateNodeWeakSet.has(this.stateNode)) {
+    //     //   stateNodeWeakSet.add(this.stateNode);
+    //     //   Spy(this.stateNode.updater, "enqueueSetState", cb);
+    //     //   Spy(this.stateNode.updater, "enqueueReplaceState", cb);
+    //     //   Spy(this.stateNode.updater, "enqueueForceUpdate", cb);
+    //     // }
+    //   }
+    // }
+
+    if (tag === 0 && !stateNode && memoizedState) {
+      // pass in "queue" obj to spy on dispatch func
+      console.log('attaching a spy', memoizedState.queue);
+      // getSet(memoizedState, "baseState");
+      // getSet(fiber.memoizedState, "baseUpdate");
+      // getSet(fiber.memoizedState, "memoizedState");
+      // getSet(fiber.memoizedState, "next");
+
+      const cb = (...args) => {
+        this.updateList.push([...args]);
+      };
+      //type is function and thus it's unique
+      // if (!testWeakSet.has(fiberNode.type)) {
+      //   testWeakSet.add(fiberNode.type);
+      //   SpyUseState(memoizedState.queue, "dispatch", cb);
+      //   SpyUseState(memoizedState.queue, "lastRenderedReducer", cb);
+      // }
+    }
+  }
+
+  addChild(treeNode) {
+    // remove other uneccessary properties
+    this.child = treeNode;
+  }
+
+  addSibling(treeNode) {
+    // if (!node) return;
+    this.sibling = treeNode;
+  }
+
+  addParent(node) {
+    // if (!node) return;
+  }
 }
 
-function recordChangesToObjField(obj, field) {
-  Object.defineProperty(obj, field, {
-    get() {
-      return this._current;
-    },
-    set(value) {
-      changes.push(parseNode(value));
-      console.log(value);
-      this._current = value;
-    },
-  });
-}
+class Tree {
+  uniqueId: number;
+  componentList: any[];
+  effectList: any[];
+  root: any;
+  // uniqueId is used to identify a fiber to then help with counting re-renders
+  // componentList
+  constructor(rootNode) {
+    this.uniqueId = fiberMap.size;
+    this.componentList = [];
+    this.effectList = [];
+    this.root = null;
+    this.processNode(rootNode, null);
+  }
 
-// used to take important details of a node and exluce circular references
-// this allows for Puppeteer to stringify the result
-function parseCompletedNode(node) {
-  delete node.child;
-  delete node.sibling;
-  return {
-    type: node?.type?.name,
-    state: node.state,
-    selfBaseDuration: node.selfBaseDuration,
-  };
+  processNode(fiberNode, previousTreeNode) {
+    // id used to reference a fiber in fiberMap
+    let id = undefined;
+    // using a unique part of each fiber to identify it.
+    // both current and alternate only 1 reference to this unique part
+    // which we can use to uniquely identify a fiber node even in the case
+    // of current and alternate switching per commit/
+    let uniquePart = undefined;
+
+    // unique part of a fiber node depends on its type.
+    if (fiberNode.tag === 0 || fiberNode.tag === 10 || fiberNode.tag === 11 || fiberNode.tag === 9 || fiberNode.tag === 15) {
+      uniquePart = fiberNode.elementType;
+    } else if (fiberNode.tag === 3) {
+      uniquePart = fiberNode.memoizedState.element.type;
+    } else if (fiberNode.tag === 7) {
+      uniquePart = fiberNode;
+    } else {
+      uniquePart = fiberNode.stateNode;
+    }
+    // if this is a unique fiber (that both "current" and "alternate" fiber represents)
+    // then add to the processedFiber to make sure we don't re-account this fiber.
+    if (!processedFibers.has(uniquePart)) {
+      // processedFibers.add(fiberNode);
+      // componentMap.set(this.uniqueId, fiberNode);
+      id = this.uniqueId;
+      this.uniqueId++;
+
+      fiberMap.set(id, fiberNode);
+      // if (fiberNode.tag === 0) {
+      //   processedFibers.set(fiberNode.elementType, id);
+      // } else if (fiberNode.tag === 3) {
+      //   processedFibers.set(fiberNode.memoizedState.element.type, id);
+      // } else {
+      //   processedFibers.set(fiberNode.stateNode, id);
+      // }
+      processedFibers.set(uniquePart, id);
+    } else {
+      id = processedFibers.get(uniquePart);
+    }
+
+    // If it's a HostRoot with a tag of 3
+    // create a new TreeNode
+    if (fiberNode.tag === 3) {
+      this.root = new TreeNode(fiberNode, id);
+      // this.root = new TreeNode(fiberNode, this.uniqueId);
+      this.componentList.push({...this.root}); // push a copy
+      // this.uniqueId++;
+
+      if (fiberNode.child) {
+        // const newNode = new TreeNode(fiberNode.child, this.uniqueId);
+        // this.root.addChild(newNode);
+        // this.componentList.push(newNode);
+        // this.uniqueId++;
+        this.processNode(fiberNode.child, this.root);
+      }
+    } else {
+      const newNode = new TreeNode(fiberNode, id);
+      // const newNode = new TreeNode(fiberNode, this.uniqueId);
+      previousTreeNode.addChild(newNode);
+      this.componentList.push({...newNode});
+      // this.uniqueId++;
+
+      if (fiberNode.child) {
+        this.processNode(fiberNode.child, newNode);
+      }
+      if (fiberNode.sibling) {
+        this.processSiblingNode(fiberNode.sibling, newNode);
+      }
+    }
+  }
+
+  processSiblingNode(fiberNode, previousTreeNode) {
+    let uniquePart = undefined;
+    let id = undefined;
+    if (fiberNode.tag === 0 || fiberNode.tag === 10 || fiberNode.tag === 11 || fiberNode.tag === 9) {
+      uniquePart = fiberNode.elementType;
+    } else if (fiberNode.tag === 3) {
+      uniquePart = fiberNode.memoizedState.element.type;
+    } else if (fiberNode.tag === 7) {
+      uniquePart = fiberNode;
+    } else {
+      uniquePart = fiberNode.stateNode;
+    }
+    if (!processedFibers.has(uniquePart)) {
+      // processedFibers.add(fiberNode);
+      // componentMap.set(this.uniqueId, fiberNode);
+      id = this.uniqueId;
+      this.uniqueId++;
+      fiberMap.set(id, fiberNode);
+      // if (fiberNode.tag === 0) {
+      //   processedFibers.set(fiberNode.elementType, id);
+      // } else if (fiberNode.tag === 3) {
+      //   processedFibers.set(fiberNode.memoizedState.element.type, id);
+      // } else {
+      //   processedFibers.set(fiberNode.stateNode, id);
+      // }
+      processedFibers.set(uniquePart, id);
+    } else {
+      id = processedFibers.get(uniquePart);
+    }
+
+    const newNode = new TreeNode(fiberNode, id);
+    // const newNode = new TreeNode(fiberNode, this.uniqueId);
+    previousTreeNode.addSibling(newNode);
+    this.componentList.push({...newNode});
+    // this.uniqueId++;
+
+    if (fiberNode.child) {
+      this.processNode(fiberNode.child, newNode);
+    }
+    if (fiberNode.sibling) {
+      this.processSiblingNode(fiberNode.sibling, newNode);
+    }
+  }
 }
 
 function mountToReactRoot(reactRoot) {
   // Reset changes
   changes = [];
 
+  function getSet(obj, propName) {
+    const newPropName = `_${propName}`;
+    obj[newPropName] = obj[propName];
+    Object.defineProperty(obj, propName, {
+      get() {
+        return this[newPropName];
+      },
+      set(newVal) {
+        this[newPropName] = newVal;
+        console.log(`${obj} ${propName}`, this[newPropName]);
+        changes.push(new Tree(this[newPropName]));
+
+        console.log('CHANGES', changes);
+        // console.log("Fiber STORE: ", processedFibers);
+        // console.log("testweakset after", testWeakSet);
+        // console.log("statenodeweakset", stateNodeWeakSet);
+        // console.log("fiber map:", fiberMap);
+        // getTotalRenderCount();
+        // whatChanged();
+      },
+    });
+  }
+
   // Lift parent of react fibers tree
   const parent = reactRoot._reactRootContainer._internalRoot;
-  const {current} = parent;
+
+  console.log('ROOT FIBER', parent.current);
+  changes.push(new Tree(parent.current));
+
+  // after initial
+  // console.log("Initial processed fibers", processedFibers);
+  console.log('initial fiberMap', fiberMap);
 
   // Add listener to react fibers tree so changes can be recorded
-  recordChangesToObjField(parent, 'current');
-
+  getSet(parent, 'current');
   // Reassign react fibers tree to record initial state
-  parent.current = current;
+  // parent.current = current;
   return changes;
-}
-
-function traverseWith(fiber, callback) {
-  callback(fiber);
-  if (fiber.child) {
-    traverseWith(fiber.child, callback);
-  }
-  if (fiber.sibling) {
-    traverseWith(fiber.sibling, callback);
-  }
-}
-
-function flattenTree(tree) {
-  // Closured array for storing fibers
-  const arr = [];
-  // Closured callback for adding to arr
-  const callback = fiber => {
-    arr.push(fiber);
-  };
-  traverseWith(tree, callback);
-  return arr;
-}
-
-function checkTime(fiber, threshold) {
-  return fiber.selfBaseDuration > threshold;
 }
 
 /**
  *
  * @param {number} threshold The rendering time to filter for.
  */
-function getAllSlowComponentRenders(threshold: number) {
-  const slowRenders = changes
-    .map(flattenTree) // Flatten tree
-    .flat() // Flatten 2d array into 1d array
-    .filter(fiber => checkTime(fiber, threshold)) // filter out all that don't meet threshold
-    .map(parseCompletedNode); // removes circular references
+function getAllSlowComponentRenders(threshold) {
+  let slowRenders = changes.map(commit => {
+    return commit.componentList.map(component => {
+      delete component.memoizedProps;
+      delete component.memoizedState;
+      delete component.updateList;
+      delete component.updateQueue;
+      delete component.ref;
+      delete component.elementType;
+      delete component.stateNode;
+
+      return {
+        ...component,
+      };
+    });
+  });
+
+  slowRenders = slowRenders.map(commit => {
+    return commit.filter(component => component.selfBaseDuration >= threshold);
+  });
+
   return slowRenders;
 }
 
-module.exports = {mountToReactRoot, getAllSlowComponentRenders, traverseWith};
+// function checkTime(fiber, threshold) {
+//   return fiber.selfBaseDuration > threshold;
+// }
+
+// function getComponentRenderTime(componentName) {
+//   console.log("changes", changes);
+//   console.log("component name", componentName);
+
+//   return "what";
+// }
+
+function getTotalRenderCount() {
+  const componentMap = new Map();
+
+  // loop through each commit
+  // for each commit, loop through the array of trees
+  // for each tree
+  // check if the current component exist in the map before adding on or creating a new key
+  changes.forEach((commit, commitIndex) => {
+    commit.componentList.forEach((component, componentIndex) => {
+      if (!componentMap.has(component.uID)) {
+        componentMap.set(component.uID, {renderCount: 1});
+      } else {
+        if (didFiberRender(changes[commitIndex ? commitIndex - 1 : 0].componentList[componentIndex], component)) {
+          componentMap.get(component.uID).renderCount += 1;
+        }
+      }
+    });
+  });
+  return componentMap;
+}
+
+function didFiberRender(prevFiber, nextFiber) {
+  switch (nextFiber.tag) {
+    case 0:
+    case 1:
+    case 3:
+      // case 5:
+      return (nextFiber.effectTag & 1) === 1;
+    default:
+      return (
+        prevFiber.memoizedProps !== nextFiber.memoizedProps ||
+        prevFiber.memoizedState !== nextFiber.memoizedState ||
+        prevFiber.ref !== nextFiber.ref
+      );
+  }
+}
+
+function didHooksChange(previous, next) {
+  if (previous == null || next == null) {
+    return false;
+  }
+  if (
+    next.hasOwnProperty('baseState') &&
+    next.hasOwnProperty('memoizedState') &&
+    next.hasOwnProperty('next') &&
+    next.hasOwnProperty('queue')
+  ) {
+    if (next.memoizedState !== previous.memoizedState) {
+      return true;
+    } else {
+    }
+  }
+  return false;
+}
+
+function getChangedKeys(previous, next) {
+  if (previous == null || next == null) {
+    return null;
+  }
+  // We can't report anything meaningful for hooks changes.
+  if (
+    next.hasOwnProperty('baseState') &&
+    next.hasOwnProperty('memoizedState') &&
+    next.hasOwnProperty('next') &&
+    next.hasOwnProperty('queue')
+  ) {
+    return null;
+  }
+
+  const keys = new Set([...Object.keys(previous), ...Object.keys(next)]);
+  const changedKeys = [];
+  // for (const key of keys) {
+  //   if (previous[key] !== next[key]) {
+  //     changedKeys.push(key);
+  //   }
+  // }
+  keys.forEach(key => {
+    if (previous[key] !== next[key]) {
+      changedKeys.push(key);
+    }
+  });
+
+  return changedKeys;
+}
+
+function getElementName(fiber) {
+  switch (fiber.tag) {
+    case 1:
+      return fiber.elementType.name;
+    case 3:
+      return 'Host Root - The element you used to render the React App';
+    case 5:
+      return `${fiber.elementType}${fiber.elementType.className ? `.${fiber.elementType.className}` : ''}`;
+    default:
+      return `${fiber.elementType}`;
+  }
+}
+
+module.exports = {mountToReactRoot, getAllSlowComponentRenders};
